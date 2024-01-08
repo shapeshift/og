0990e7a183f7461f4ad59120ca5295f44875bf11
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-01-08 16:26:34 +0100

    feat: add chakra UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import './index.css'
 
+import { ChakraProvider, createLocalStorageManager } from '@chakra-ui/react'
 import App from 'App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+const manager = createLocalStorageManager('ss-theme')
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider colorModeManager={manager} cssVarsRoot='body'>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
