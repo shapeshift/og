262601a55a36a0cd92d1bcd60412031294fa8a14
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-01-08 14:02:53 +0100

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 // eslint-disable-next-line import/no-default-export
@@ -7,5 +8,5 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [react()],
+  plugins: [tsconfigPaths(), react()],
 })
