4c531e84467343e68fc340d49200581adbf2525b
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-01-08 13:49:31 +0100

    feat: add vitest

---
+++ .vitest.config.ts
@@ -0,0 +1,27 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  test: {
+    root: process.cwd(),
+    passWithNoTests: true, // so we can confirm the test script is working with no tests yet
+    include: [
+      './**/*.test.ts',
+      './**/*.test.tsx',
+    ],
+    environment: 'happy-dom',
+    setupFiles: [],
+    clearMocks: true,
+    poolOptions: {
+      isolate: false,
+      threads: {
+        singleThread: true,
+      },
+      forks: {
+        isolate: false,
+      },
+    },
+  },
+})
