448f08e6b2d9929aeb243707677d8204098b21b3
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2024-01-09 15:54:50 +1100

    feat: add chatwoot

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,9 @@ interface ImportMetaEnv {
   readonly VITE_FOO: string
   readonly VITE_FEATURE_MIXPANEL: boolean
   readonly VITE_MIXPANEL_TOKEN: string
+  readonly VITE_CHATWOOT_URL: string
+  readonly VITE_CHATWOOT_TOKEN: string
+  readonly VITE_FEATURE_CHATWOOT: boolean
 }
 
 interface ImportMeta {
