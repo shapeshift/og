ea4898bf0e4979e1615dff041fe9daf31f402675
gomes <17035424+gomesalexandre@users.noreply.github.com>
2025-01-29 14:43:24 +0100

    feat: remove defaultOptions

--- src/config/react-query.ts
+++ src/config/react-query.ts
@@ -1,11 +1,3 @@
 import { QueryClient } from '@tanstack/react-query'
 
-export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 5, // 5 minutes
-      gcTime: 1000 * 60 * 60 * 24, // 24 hours
-      retry: 3,
-    },
-  },
-})
+export const queryClient = new QueryClient()
