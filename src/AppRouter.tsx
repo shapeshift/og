8350efbd092082f71159de77997246a79f49f65c
gomes <17035424+gomesalexandre@users.noreply.github.com>
2025-02-10 19:31:13 +0100

    feat: styles

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -56,6 +56,7 @@ const bgContainerSx = {
 
 const headingFontSizeSx = { base: '40px', md: '56px' }
 const headingLineHeightSx = { base: '48px', md: '64px' }
+const contentPt = { base: '6vh', md: '8vh' }
 
 const getVariants = (pathname: string) => {
   // Not sure what this does but this looks good on status screen so
@@ -119,75 +120,73 @@ export const AppRouter = () => {
 
   return (
     <FormProvider {...methods}>
-      <Center
-        width='full'
-        height='100vh'
-        flexDir='column'
-        bgImage="url('/src/assets/bg.jpg')"
-        bgSize='cover'
-        bgPosition='center'
-        bgRepeat='no-repeat'
-        position='relative'
-        sx={bgContainerSx}
-      >
-        <Box
-          position='relative'
-          zIndex={1}
-          width='full'
-          display='flex'
-          justifyContent='center'
-          flexDir='column'
-          alignItems='center'
-        >
-          <Box mb={16} textAlign='center' maxWidth='container.sm' mx='auto'>
-            <Heading
-              fontSize={headingFontSizeSx}
-              lineHeight={headingLineHeightSx}
-              fontWeight='semibold'
-              mb={4}
-              letterSpacing='-0.02em'
-              display='flex'
-              flexDirection='column'
-              alignItems='center'
-            >
-              <span>Ditch your wallet,</span>
-              <span>Swap multichain</span>
-            </Heading>
-            <Text
-              fontSize='16px'
-              color='whiteAlpha.700'
-              maxWidth='480px'
-              mx='auto'
-              textAlign='center'
-              lineHeight='1.6'
-              letterSpacing='-0.01em'
-            >
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua.
-            </Text>
+      <Box width='full' height='100vh' position='relative'>
+        {/* Blurry fancy bg underlay */}
+        <Center
+          position='absolute'
+          top={0}
+          left={0}
+          right={0}
+          bottom={0}
+          bgImage="url('/src/assets/bg.jpg')"
+          bgSize='cover'
+          bgPosition='center'
+          bgRepeat='no-repeat'
+          sx={bgContainerSx}
+        />
+        {/* Akschual content overlay */}
+        <Box position='relative' zIndex={1} width='full' height='full' pt={contentPt}>
+          <Box width='full' display='flex' flexDir='column' alignItems='center'>
+            <Box mb={12} textAlign='center' maxWidth='container.sm' mx='auto'>
+              <Heading
+                fontSize={headingFontSizeSx}
+                lineHeight={headingLineHeightSx}
+                fontWeight='semibold'
+                mb={4}
+                letterSpacing='-0.02em'
+                display='flex'
+                flexDirection='column'
+                alignItems='center'
+              >
+                <span>Ditch your wallet,</span>
+                <span>Swap multichain</span>
+              </Heading>
+              <Text
+                fontSize='16px'
+                color='whiteAlpha.700'
+                maxWidth='480px'
+                mx='auto'
+                textAlign='center'
+                lineHeight='1.6'
+                letterSpacing='-0.01em'
+              >
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua.
+              </Text>
+            </Box>
+            <AnimatePresence mode='wait' initial={false}>
+              <motion.div
+                key={location.pathname}
+                initial='initial'
+                animate='animate'
+                exit='exit'
+                variants={getVariants(location.pathname)}
+                transition={transition}
+                style={motionWrapperSx}
+              >
+                <Routes location={location}>
+                  <Route path='/' element={selectPair} />
+                  <Route path='/input' element={tradeInput} />
+                  <Route path='/status' element={status} />
+                </Routes>
+              </motion.div>
+            </AnimatePresence>
           </Box>
-          <AnimatePresence mode='wait' initial={false}>
-            <motion.div
-              key={location.pathname}
-              initial='initial'
-              animate='animate'
-              exit='exit'
-              variants={getVariants(location.pathname)}
-              transition={transition}
-              style={motionWrapperSx}
-            >
-              <Routes location={location}>
-                <Route path='/' element={selectPair} />
-                <Route path='/input' element={tradeInput} />
-                <Route path='/status' element={status} />
-              </Routes>
-            </motion.div>
-          </AnimatePresence>
+          <Box position='absolute' bottom={4} right={4} zIndex={1}>
+            <ChatwootButton />
+          </Box>
         </Box>
-        <Box position='absolute' bottom={4} right={4} zIndex={1}>
-          <ChatwootButton />
-        </Box>
-      </Center>
+      </Box>
     </FormProvider>
   )
 }
