e6a7a207e2a2beaec74fa61044ad57bab504400c
gomes <17035424+gomesalexandre@users.noreply.github.com>
2025-03-11 19:13:53 +0100

    feat: release (#84)

--- src/lib/mixpanel/types.ts
+++ src/lib/mixpanel/types.ts
@@ -5,4 +5,7 @@ export type MixPanelType = typeof Mixpanel
 export enum MixPanelEvent {
   PairSelected = 'Pair Selected',
   StartTransaction = 'Start Transaction',
+  SwapCompleted = 'Swap Completed',
+  DoAnotherSwap = 'Do Another Swap',
+  LaunchShapeshiftApp = 'Launch Shapeshift App',
 }
